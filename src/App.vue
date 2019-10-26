541a5fa099ced620d8875bc3da49247cbe2e9b74
codeshootah <vnukovg@gmail.com>
2019-10-26 14:50:33 +0300

    add product

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/" v-text="brand"></router-link>
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      brand: "Webstore",
+      roduct: {
+        id: 1,
+        title: "Product 1",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, animi.",
+        price: 99,
+        image: 'assets/images/product-1.png'
+      }
+    };
+  }
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -23,6 +41,7 @@
   a {
     font-weight: bold;
     color: #2c3e50;
+    padding: 0 10px;
 
     &.router-link-exact-active {
       color: #42b983;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+
+    <div class="product">
+      <figure class="image">
+        <img v-bind:src="product.image">
+      </figure>
+      <div>
+        <h2 v-text="product.title"></h2>
+        <p v-text="product.description"></p>
+        <p v-text="product.price + ' $'" class="price"></p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      product: {
+        id: 1,
+        title: "Product 1",
+        description:
+          "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, animi.",
+        price: 99,
+        image: require("@/assets/images/product-1.png")
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.product {
+  width: 25%;
+  text-align: left;
+  figure {
+    margin: 0;
+  }
+  img {
+    width: 100%;
   }
 }
-</script>
+</style>
